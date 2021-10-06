2ff1757fe7a918fa72502c1e60d9c2d84cefa028
Miyuru <miyurushalitha@gmail.com>
2021-10-07 01:15:35 +0530

    Add basic Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
+import resume from "../assets/resume/resume.txt";
 
 function Header() {
   return (
     <header className="header">
-      <h1>HEADER</h1>
+      <div className="header__text-box u-margin-bottom-medium">
+        <h1 className="heading-primary">Hi! I Am Miyuru Shalitha</h1>
+      </div>
+
+      <a download="resume.txt" href={resume} className="btn btn--white">
+        Download Resume
+      </a>
+
+      {/* Add linkdin and github icons */}
     </header>
   );
 }
